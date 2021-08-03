4f36fae58be97ffd84cbf972e61afb28c0b2f8c8
lancebrudnicki <lancebrudnicki@gmail.com>
2021-08-03 13:48:05 -0600

    main

---
+++ HumanTest.java
@@ -0,0 +1,12 @@
+package com.objectmaster;
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		Human human1 = new Human();
+		Human human2 = new Human();
+		human1.attack(human2);
+		
+	}
+
+}
